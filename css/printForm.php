684ffb97b086407558c714fe0b557fb10ee82873
miatwan <miatwan@gmail.com>
2022-07-17 13:03:06 +0300

    assets

---
+++ css/printForm.php
@@ -0,0 +1,117 @@
+<style type="text/css">
+
+    .table1
+    {
+        width:  90%;
+        padding-top: 55px;
+        border:none;
+        align-content: center;
+        font-size: 23px;
+    }
+
+    .table2
+    {
+        width:  90%;
+        border:none;
+        align-content: center;
+    }
+
+
+    .table3
+    {
+        width:  100%;
+        border:none;
+        align-content: center;
+    }
+
+
+    .table4
+    {
+        width:  96%;
+        border:none;
+        align-content: center;
+    }
+
+
+
+    table.page_footer {width: 100%; border: none; background-color: #ffffff; border-top: solid 1mm #cccccc; padding: 2mm}
+
+    .th1
+    {
+        text-align: left;
+
+    }
+
+    .td1
+    {
+        text-align: left;
+        border:none;
+        font-size: 19px;
+        font-weight: bold;
+        background-color: #00AA33;
+    }
+
+    .note1{
+        font-size: 3ex;
+        color: #4f5962;
+    }
+
+    td.col1
+    {
+        border:none;
+        text-align: right;
+    }
+
+    .filed{
+        font-size: 30px;
+        color: #9a161a;
+    }
+
+    .sucsess{
+        font-size: 30px;
+        color: #0caa1c;
+    }
+
+    .hr1
+    {
+       height: 1px;
+    }
+
+    .smallHint
+    {
+        font-size: 9px;
+    }
+
+    .borders1{
+        border-right: #0c0d0e;
+        border-width: 1px;
+        border-top: #0c0d0e;
+        border-bottom: #0c0d0e;
+    }
+
+
+
+    .borders2{
+        border-right: #0c0d0e;
+        border-width: 1px;
+        border-bottom: #0c0d0e;
+    }
+
+
+    .borders3{
+        border-right: #0c0d0e;
+        border-width: 1px;
+        border-bottom: #0c0d0e;
+    }
+
+
+    .borders4{
+        border-width: 1px;
+        border-bottom: #0c0d0e;
+    }
+
+    .tohomaFont{
+        font-family: ALmusam_free;
+    }
+
+</style>
